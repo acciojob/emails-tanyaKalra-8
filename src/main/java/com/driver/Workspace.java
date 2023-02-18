57f95cb5ffa65bfe3b2f70aacc921da8c26dca15
Taniya <tanya.kalra200@gmail.com>
2023-02-18 15:15:59 +0530

    testing

--- src/main/java/com/driver/Workspace.java
+++ src/main/java/com/driver/Workspace.java
@@ -21,6 +21,29 @@ public class Workspace extends Gmail{
         calendar.add(meeting);
     }
 
+//    public int findMaxMeetings(){
+//        // find the maximum number of meetings you can attend
+//        // 1. At a particular time, you can be present in at most one meeting
+//        // 2. If you want to attend a meeting, you must join it at its start time and leave at end time.
+//        // Example: If a meeting ends at 10:00 am, you cannot attend another meeting starting at 10:00 am
+//        int meetingCounts=0;
+//        if (calendar.size()==0) return 0;
+//        if (calendar.size()==1) return 1;
+//        for (int i=0;i<calendar.size()-1;i++){
+//            LocalTime start1 = calendar.get(i).getStartTime();
+//            LocalTime end1 = calendar.get(i).getEndTime();
+//
+//            LocalTime start2 = calendar.get(i+1).getStartTime();
+//            LocalTime end2 = calendar.get(i+1).getEndTime();
+//
+//            if(!start1.equals(start2)){
+//                meetingCounts++;
+//            }
+//        }
+//        return meetingCounts;
+//    }
+//}
+
     public int findMaxMeetings(){
         // find the maximum number of meetings you can attend
         // 1. At a particular time, you can be present in at most one meeting
@@ -29,16 +52,27 @@ public class Workspace extends Gmail{
         int meetingCounts=0;
         if (calendar.size()==0) return 0;
         if (calendar.size()==1) return 1;
-        for (int i=0;i<calendar.size()-1;i++){
-            LocalTime start1 = calendar.get(i).getStartTime();
-            LocalTime end1 = calendar.get(i).getEndTime();
 
-            LocalTime start2 = calendar.get(i+1).getStartTime();
-            LocalTime end2 = calendar.get(i+1).getEndTime();
+        LocalTime start1 = calendar.get(0).getStartTime();
+        LocalTime end1 = calendar.get(0).getEndTime();
+        meetingCounts++;
 
-            if(!start1.equals(start2)){
-                meetingCounts++;
+        for (int i=1;i<calendar.size();i++){
+            LocalTime start = calendar.get(i).getStartTime();
+            LocalTime end = calendar.get(i).getEndTime();
+
+            if(start.equals(start1)){
+                continue;
             }
+            else if (start.isAfter(start1) && start.isBefore(end1)){
+                continue;
+            }
+            else if(start.isBefore(start1) && end.isAfter(start1)){
+                continue;
+            }
+            start1 = start;
+            end1 = end;
+            meetingCounts++;
         }
         return meetingCounts;
     }
